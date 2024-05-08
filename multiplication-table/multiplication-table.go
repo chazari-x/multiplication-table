8813c7c7c47268fe0710b0494374069ab45df23b
chazari <chalimovrz@gmail.com>
2024-05-08 19:09:38 +0600

    comments

--- multiplication-table/multiplication-table.go
+++ multiplication-table/multiplication-table.go
@@ -6,21 +6,28 @@ import (
 )
 
 func MultiplicationTable(count int) {
+	// Подготовка формата вывода
 	var l = strconv.Itoa(len(strconv.Itoa(count*count)) + 1)
 	var format = "%" + l + "s"
 
+	// Подготовка таблицы
 	var res = [][]string{{" "}}
+	// Заполнение первой строки
 	for i := 1; i <= count; i++ {
 		res[0] = append(res[0], strconv.Itoa(i))
 	}
 
+	// Заполнение таблицы
 	for i := 1; i <= count; i++ {
+		// Добавление строки
 		res = append(res, []string{strconv.Itoa(i)})
+		// Заполнение строки
 		for k := 1; k <= count; k++ {
 			res[i] = append(res[i], strconv.Itoa(k*(i)))
 		}
 	}
 
+	// Вывод таблицы
 	for _, r1 := range res {
 		for _, r2 := range r1 {
 			fmt.Printf(format, r2)
